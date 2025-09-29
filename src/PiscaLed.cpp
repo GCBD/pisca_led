01015e080effcd578919f0c0c5de72199bf2ba72
Gilberto Carlos <gcbd3@yahoo.com>
2025-09-28 21:55:55 -0300

    Este é um repositório (pasta) na nuvem

---
+++ src/PiscaLed.cpp
@@ -0,0 +1,21 @@
+#include <PiscaLed.h>
+
+// variáveis globais
+int _pino;
+int _time;
+
+PiscaLed::PiscaLed(int pino, long time)
+{
+    // pino e time só podem serem acessados de dentro dessa função
+    pinMode(pino, OUTPUT);
+    _pino = pino;
+    _time = time;
+}
+
+void PiscaLed::acende_led()
+{
+    digitalWrite(_pino, HIGH);
+    delay(_time);
+    digitalWrite(_pino, LOW);
+    delay(_time);
+}
